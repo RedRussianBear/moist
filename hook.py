e6dce025aa870bba0c8d263580f648ab8da23ce0
Mikhail Khrenov <mkhrenov34@gmail.com>
2017-01-14 22:55:10 -0500

    Cut hook.y

--- hook.py
+++
@@ -1,6 +0,0 @@
-from subprocess import call
-file = open('/home/moist/git/moist/message.txt', 'w')
-file.write("This works!")
-file.close()
-call(["cd", "/home/moist/git/moist/"])
-call(["git", "push"])
